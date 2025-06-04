900b746abdb529e2305a2f0b76a411bd003e201f
TheSisb <TheSisb@users.noreply.github.com>
2025-06-04 02:30:06 +0000

    Deploying to gh-pages from  @ 9bc49d58e8ce902e6ba6aac93eb323e7a4af519f 🚀

---
+++ docs/assets/add-column.stories-3VmdKcuU.js
@@ -0,0 +1,14 @@
+import{j as o,a as c,F as p}from"./marked.esm-I79JNzkl.js";import"./index-KtDdPitk.js";import{D as u}from"./data-editor-all-VGlS32hr.js";import{B as d,D as C,M as g,u as h,d as f}from"./utils-TNBvQJ4i.js";import{S as D}from"./story-utils-yBezD1KV.js";import"./iframe-kvLG9FGK.js";import"../sb-preview/runtime.js";import"./image-window-loader-HmD2rGpR.js";import"./throttle-UPaD_CsS.js";import"./_baseIteratee-MFXT5464.js";import"./flatten-5Fd1fcTu.js";import"./scrolling-data-grid-_nOSqJ4f.js";import"./index-PWBWJyi_.js";import"./index.esm-iAJ67Rxz.js";import"./index-iY2XSz2k.js";const P={title:"Glide-Data-Grid/DataEditor Demos",decorators:[t=>o(D,{children:o(d,{title:"Add and remove columns",description:c(p,{children:[o(C,{children:"You can add and remove columns at your disposal"}),o(g,{children:"Use the story's controls to change the number of columns"})]}),children:o(t,{})})})]},r=t=>{const{cols:a,getCellContent:l}=h(t.columnsCount),m=a.map(i=>({...i,contentAlign:"right",icon:null}));return o(u,{...f,rowMarkers:"number",getCellContent:l,experimental:{strict:!0},columns:m,rows:1e4})};r.args={columnsCount:10};r.argTypes={columnsCount:{control:{type:"range",min:2,max:200}}};var e,n,s;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`p => {
+  const {
+    cols,
+    getCellContent
+  } = useMockDataGenerator(p.columnsCount);
+  const newCols = cols.map(c => ({
+    ...c,
+    contentAlign: "right",
+    icon: null
+  }));
+  return <DataEditor {...defaultProps} rowMarkers="number" getCellContent={getCellContent} experimental={{
+    strict: true
+  }} columns={newCols} rows={10_000} />;
+}`,...(s=(n=r.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const T=["AddColumns"];export{r as AddColumns,T as __namedExportsOrder,P as default};
---
+++ docs/assets/content-alignment.stories-T71rkdZ0.js
@@ -0,0 +1,29 @@
+import{j as t,a as p}from"./marked.esm-I79JNzkl.js";import{R as C}from"./index-KtDdPitk.js";import{D as d}from"./data-editor-all-VGlS32hr.js";import{B as g,D as u,P as o,a as f,d as A}from"./utils-TNBvQJ4i.js";import{S as h}from"./story-utils-yBezD1KV.js";import"./iframe-kvLG9FGK.js";import"../sb-preview/runtime.js";import"./image-window-loader-HmD2rGpR.js";import"./throttle-UPaD_CsS.js";import"./_baseIteratee-MFXT5464.js";import"./flatten-5Fd1fcTu.js";import"./scrolling-data-grid-_nOSqJ4f.js";import"./index-PWBWJyi_.js";import"./index.esm-iAJ67Rxz.js";import"./index-iY2XSz2k.js";const W={title:"Glide-Data-Grid/DataEditor Demos",decorators:[l=>t(h,{children:t(g,{title:"Content Alignment",description:p(u,{children:["You can customize the content alignment by setting ",t(o,{children:"contentAlign"})," of a cell to ",t(o,{children:"left"}),", ",t(o,{children:"right"})," or ",t(o,{children:"center"}),"."]}),children:t(l,{})})})]},r=()=>{const{cols:l,getCellContent:n}=f(),m=C.useCallback(e=>{const[i,D]=e;return i===3?{...n(e),contentAlign:"center"}:i===4?{...n(e),contentAlign:"right"}:i===5?{...n(e),contentAlign:"left"}:n(e)},[n]);return t(d,{...A,getCellContent:m,columns:l,rows:300})};var c,s,a;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`() => {
+  const {
+    cols,
+    getCellContent
+  } = useAllMockedKinds();
+  const mangledGetCellContent = React.useCallback<typeof getCellContent>(cell => {
+    const [col, _row] = cell;
+    if (col === 3) {
+      return {
+        ...getCellContent(cell),
+        contentAlign: "center"
+      };
+    }
+    if (col === 4) {
+      return {
+        ...getCellContent(cell),
+        contentAlign: "right"
+      };
+    }
+    if (col === 5) {
+      return {
+        ...getCellContent(cell),
+        contentAlign: "left"
+      };
+    }
+    return getCellContent(cell);
+  }, [getCellContent]);
+  return <DataEditor {...defaultProps} getCellContent={mangledGetCellContent} columns={cols} rows={300} />;
+}`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const z=["ContentAlignment"];export{r as ContentAlignment,z as __namedExportsOrder,W as default};
