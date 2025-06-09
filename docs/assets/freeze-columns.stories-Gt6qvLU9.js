0052205a29fa74de8d353577f765fe7aa942ae50
TheSisb <TheSisb@users.noreply.github.com>
2025-06-09 19:49:13 +0000

    Deploying to gh-pages from  @ 58ead297750e2d2c70cce0ab542e2fac939c7cea 🚀

--- docs/assets/freeze-columns.stories-Gt6qvLU9.js
+++
@@ -1,9 +0,0 @@
-import{j as r,a as m}from"./marked.esm-I79JNzkl.js";import"./index-KtDdPitk.js";import{D as i}from"./data-editor-all-VGlS32hr.js";import{B as p,D as c,P as u,u as d,d as f}from"./utils-TNBvQJ4i.js";import{S as C}from"./story-utils-yBezD1KV.js";import"./iframe-kvLG9FGK.js";import"../sb-preview/runtime.js";import"./image-window-loader-HmD2rGpR.js";import"./throttle-UPaD_CsS.js";import"./_baseIteratee-MFXT5464.js";import"./flatten-5Fd1fcTu.js";import"./scrolling-data-grid-_nOSqJ4f.js";import"./index-PWBWJyi_.js";import"./index.esm-iAJ67Rxz.js";import"./index-iY2XSz2k.js";const F={title:"Glide-Data-Grid/DataEditor Demos",decorators:[o=>r(C,{children:r(p,{title:"Freeze columns",description:m(c,{children:["Columns at the start of your grid can be frozen in place by settings"," ",r(u,{children:"freezeColumns"})," to a number greater than 0."]}),children:r(o,{})})})]},e=o=>{const{cols:n,getCellContent:l}=d(100);return r(i,{...f,rowMarkers:"both",freezeColumns:o.freezeColumns,getCellContent:l,columns:n,verticalBorder:!1,rows:1e3})};e.argTypes={freezeColumns:{control:{type:"range",min:0,max:10}}};e.args={freezeColumns:1};var t,s,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`(p: {
-  freezeColumns: number;
-}) => {
-  const {
-    cols,
-    getCellContent
-  } = useMockDataGenerator(100);
-  return <DataEditor {...defaultProps} rowMarkers="both" freezeColumns={p.freezeColumns} getCellContent={getCellContent} columns={cols} verticalBorder={false} rows={1000} />;
-}`,...(a=(s=e.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const S=["FreezeColumns"];export{e as FreezeColumns,S as __namedExportsOrder,F as default};
--- docs/assets/ten-million-cells.stories-OIzygjle.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./marked.esm-I79JNzkl.js";import"./index-KtDdPitk.js";import{D as n}from"./data-editor-all-VGlS32hr.js";import{B as a,D as m,u as p,d as c}from"./utils-TNBvQJ4i.js";import{S as d}from"./story-utils-yBezD1KV.js";import"./iframe-kvLG9FGK.js";import"../sb-preview/runtime.js";import"./image-window-loader-HmD2rGpR.js";import"./throttle-UPaD_CsS.js";import"./_baseIteratee-MFXT5464.js";import"./flatten-5Fd1fcTu.js";import"./scrolling-data-grid-_nOSqJ4f.js";import"./index-PWBWJyi_.js";import"./index.esm-iAJ67Rxz.js";import"./index-iY2XSz2k.js";const b={title:"Glide-Data-Grid/DataEditor Demos",decorators:[o=>r(d,{children:r(a,{title:"Ten Million Cells",description:r(m,{children:"Data grid supports over 10 million cells. Go nuts with it."}),children:r(o,{})})})]},e=()=>{const{cols:o,getCellContent:l}=p(100);return r(n,{...c,rowMarkers:"number",getCellContent:l,columns:o,rows:1e5})};var t,s,i;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`() => {
-  const {
-    cols,
-    getCellContent
-  } = useMockDataGenerator(100);
-  return <DataEditor {...defaultProps} rowMarkers="number" getCellContent={getCellContent} columns={cols} rows={100_000} />;
-}`,...(i=(s=e.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const j=["TenMillionCells"];export{e as TenMillionCells,j as __namedExportsOrder,b as default};
