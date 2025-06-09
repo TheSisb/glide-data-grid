55ecf697ef9a602629770ef906c87673a9446e25
TheSisb <TheSisb@users.noreply.github.com>
2025-06-09 20:58:07 +0000

    Deploying to gh-pages from  @ 5d33233f9e80580ee75bfac328182ea5efbb84db 🚀

---
+++ docs/assets/resizable-columns.stories-LaM7zQOj.js
@@ -0,0 +1,12 @@
+import{j as e,a as t,F as c}from"./marked.esm-I79JNzkl.js";import{R as u}from"./index-KtDdPitk.js";import{D as p}from"./data-editor-all-dapGHCxi.js";import{B as d,D as h,P as s,M as C,u as g,d as f}from"./utils-LjRUa34z.js";import{S as z}from"./story-utils-yBezD1KV.js";import"./iframe-L4-qsy29.js";import"../sb-preview/runtime.js";import"./image-window-loader-DoksJUiP.js";import"./throttle-UPaD_CsS.js";import"./_baseIteratee-MFXT5464.js";import"./flatten-5Fd1fcTu.js";import"./scrolling-data-grid-bIZpknlR.js";import"./index-PWBWJyi_.js";import"./index.esm-iAJ67Rxz.js";import"./index-iY2XSz2k.js";const Y={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>e(z,{children:e(d,{title:"Resizable columns",description:t(c,{children:[t(h,{children:["You can resize columns by dragging their edges, as long as you respond to the"," ",e(s,{children:"onColumnResize"})," prop."]}),t(C,{children:["By setting the ",e(s,{children:"overscrollX"})," property extra space can be allocated at the end of the grid to allow for easier resizing of the final column. You can highlight multiple columns to resize them all at once."]})]}),children:e(r,{})})})]},o=()=>{const{cols:r,getCellContent:i,onColumnResize:m}=g(60);return e(p,{...f,getCellContent:i,columns:r,rowMarkers:"both",overscrollX:200,overscrollY:200,maxColumnAutoWidth:500,maxColumnWidth:2e3,rows:50,scaleToRem:!0,theme:u.useMemo(()=>({baseFontStyle:"0.8125rem",headerFontStyle:"600 0.8125rem",editorFontSize:"0.8125rem"}),[]),onColumnResize:m})};var a,n,l;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`() => {
+  const {
+    cols,
+    getCellContent,
+    onColumnResize
+  } = useMockDataGenerator(60);
+  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} rowMarkers="both" overscrollX={200} overscrollY={200} maxColumnAutoWidth={500} maxColumnWidth={2000} rows={50} scaleToRem={true} theme={React.useMemo(() => ({
+    baseFontStyle: "0.8125rem",
+    headerFontStyle: "600 0.8125rem",
+    editorFontSize: "0.8125rem"
+  }), [])} onColumnResize={onColumnResize} />;
+}`,...(l=(n=o.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};const j=["ResizableColumns"];export{o as ResizableColumns,j as __namedExportsOrder,Y as default};
---
+++ docs/assets/scaled-view.stories-nffJevHz.js
@@ -0,0 +1,8 @@
+import{j as e}from"./marked.esm-I79JNzkl.js";import"./index-KtDdPitk.js";import{D as l}from"./data-editor-all-dapGHCxi.js";import{B as m,D as p,u as c,d}from"./utils-LjRUa34z.js";import{S as u}from"./story-utils-yBezD1KV.js";import"./iframe-L4-qsy29.js";import"../sb-preview/runtime.js";import"./image-window-loader-DoksJUiP.js";import"./throttle-UPaD_CsS.js";import"./_baseIteratee-MFXT5464.js";import"./flatten-5Fd1fcTu.js";import"./scrolling-data-grid-bIZpknlR.js";import"./index-PWBWJyi_.js";import"./index.esm-iAJ67Rxz.js";import"./index-iY2XSz2k.js";const V={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>e(u,{children:e(m,{title:"Scaled view",description:e(p,{children:"The data editor supports being scaled."}),scale:"0.5",children:e(r,{})})})]},o=()=>{const{cols:r,getCellContent:i,onColumnResize:n}=c(60);return e(l,{...d,getCellContent:i,columns:r,rowMarkers:"both",rows:500,onColumnResize:n})};var t,s,a;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`() => {
+  const {
+    cols,
+    getCellContent,
+    onColumnResize
+  } = useMockDataGenerator(60);
+  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} rowMarkers="both" rows={500} onColumnResize={onColumnResize} />;
+}`,...(a=(s=o.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const _=["ScaledView"];export{o as ScaledView,_ as __namedExportsOrder,V as default};
