e2992072c5fd637958835aca4fb4c8e6b0ddd368
TheSisb <TheSisb@users.noreply.github.com>
2025-12-04 03:34:23 +0000

    Deploying to gh-pages from  @ 83f2893791200366a069a0901fbce2fa77fded61 🚀

--- docs/assets/silly-numbers.stories-BctJfBRG.js
+++
@@ -1,7 +0,0 @@
-import{R as e}from"./iframe-BkLafyFo.js";import{D as n}from"./data-editor-all-Czt43zOW.js";import{B as i,D as m,u as c,d as p}from"./utils-BlJrF2Uv.js";import{S as u}from"./story-utils-DwjdYrNq.js";import"./preload-helper-C1FmrZbK.js";import"./image-window-loader-d6QkMzYP.js";import"./throttle-D7LZbMDU.js";import"./marked.esm-S5KBmSXG.js";import"./flatten-fVc8qxi7.js";import"./scrolling-data-grid-CqVRLVSU.js";import"./index-D_kXk1yT.js";import"./throttle--dN168Gr.js";const _={title:"Glide-Data-Grid/DataEditor Demos",decorators:[t=>e.createElement(u,null,e.createElement(i,{title:"100 Million Rows",description:e.createElement(m,null,"100 million rows is silly. Once we cross about 33 million pixels in height we can no longer trust the browser to scroll accurately.")},e.createElement(t,null)))]},r=()=>{const{cols:t,getCellContent:a}=c(6);return e.createElement(n,{...p,getCellContent:a,columns:t,rowHeight:31,rows:1e8,rowMarkers:"number"})};var o,s,l;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
-  const {
-    cols,
-    getCellContent
-  } = useMockDataGenerator(6);
-  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} rowHeight={31} rows={100_000_000} rowMarkers="number" />;
-}`,...(l=(s=r.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};const k=["SillyNumbers"];export{r as SillyNumbers,k as __namedExportsOrder,_ as default};
--- docs/assets/ten-million-cells.stories-BjTmav2M.js
+++
@@ -1,7 +0,0 @@
-import{R as e}from"./iframe-BkLafyFo.js";import{D as n}from"./data-editor-all-Czt43zOW.js";import{B as i,D as m,u as p,d as c}from"./utils-BlJrF2Uv.js";import{S as u}from"./story-utils-DwjdYrNq.js";import"./preload-helper-C1FmrZbK.js";import"./image-window-loader-d6QkMzYP.js";import"./throttle-D7LZbMDU.js";import"./marked.esm-S5KBmSXG.js";import"./flatten-fVc8qxi7.js";import"./scrolling-data-grid-CqVRLVSU.js";import"./index-D_kXk1yT.js";import"./throttle--dN168Gr.js";const S={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>e.createElement(u,null,e.createElement(i,{title:"Ten Million Cells",description:e.createElement(m,null,"Data grid supports over 10 million cells. Go nuts with it.")},e.createElement(r,null)))]},t=()=>{const{cols:r,getCellContent:a}=p(100);return e.createElement(n,{...c,rowMarkers:"number",getCellContent:a,columns:r,rows:1e5})};var o,l,s;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
-  const {
-    cols,
-    getCellContent
-  } = useMockDataGenerator(100);
-  return <DataEditor {...defaultProps} rowMarkers="number" getCellContent={getCellContent} columns={cols} rows={100_000} />;
-}`,...(s=(l=t.parameters)==null?void 0:l.docs)==null?void 0:s.source}}};const b=["TenMillionCells"];export{t as TenMillionCells,b as __namedExportsOrder,S as default};
