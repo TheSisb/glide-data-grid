056e7dfba4aa91b925d226f8ed705666e7c06c94
TheSisb <TheSisb@users.noreply.github.com>
2025-12-09 17:34:30 +0000

    Deploying to gh-pages from  @ 451716154f64e9930bfb3eb9136ef81d566f1e06 🚀

---
+++ docs/assets/prevent-diagonal-scroll.stories-s-FI9R__.js
@@ -0,0 +1,7 @@
+import{R as e}from"./iframe-DDQQ0Vrs.js";import{D as s}from"./data-editor-all-WfwmghEn.js";import{B as i,D as c,P as m,u as p,d as u}from"./utils-C70ZVbBO.js";import{S as g}from"./story-utils-Fo5kT6Ca.js";import"./preload-helper-C1FmrZbK.js";import"./image-window-loader-mJd1ySGt.js";import"./throttle-BwIf-uOO.js";import"./marked.esm-BilUkw4s.js";import"./flatten-rrhK1q8h.js";import"./scrolling-data-grid-DrsOBUUE.js";import"./index-D_kXk1yT.js";import"./throttle--dN168Gr.js";const w={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>e.createElement(g,null,e.createElement(i,{title:"Prevent Diagonal Scroll",description:e.createElement(e.Fragment,null,e.createElement(c,null,"Diagonal scrolling can be prevented by setting"," ",e.createElement(m,null,"preventDiagonalScrolling"),"."))},e.createElement(r,null)))]},t=()=>{const{cols:r,getCellContent:n}=p(200);return e.createElement(s,{...u,getCellContent:n,columns:r,preventDiagonalScrolling:!0,rows:5e3})};var o,a,l;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
+  const {
+    cols,
+    getCellContent
+  } = useMockDataGenerator(200);
+  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} preventDiagonalScrolling={true} rows={5000} />;
+}`,...(l=(a=t.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const x=["PreventDiagonalScroll"];export{t as PreventDiagonalScroll,x as __namedExportsOrder,w as default};
---
+++ docs/assets/small-editable-grid.stories-DQ5_59_8.js
@@ -0,0 +1,32 @@
+import{R as t}from"./iframe-DDQQ0Vrs.js";import{G as m}from"./image-window-loader-mJd1ySGt.js";import{D as c}from"./data-editor-all-WfwmghEn.js";import{B as p,D as C,u as f,d as g}from"./utils-C70ZVbBO.js";import{S as E}from"./story-utils-Fo5kT6Ca.js";import"./preload-helper-C1FmrZbK.js";import"./throttle-BwIf-uOO.js";import"./marked.esm-BilUkw4s.js";import"./flatten-rrhK1q8h.js";import"./scrolling-data-grid-DrsOBUUE.js";import"./index-D_kXk1yT.js";import"./throttle--dN168Gr.js";const B={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>t.createElement(E,null,t.createElement(p,{title:"Editable Grid",description:t.createElement(C,null,"Data grid supports overlay editors for changing values. There are bespoke editors for numbers, strings, images, booleans, markdown, and uri.")},t.createElement(r,null)))]},l=()=>{const{cols:r,getCellContent:s,setCellValue:i}=f(7,!1),d=e=>{const[u,D]=e;return u===6?{kind:m.Number,data:100,displayData:"100",readonly:!1,allowOverlay:!0,allowNegative:!0}:s(e)};return t.createElement(c,{...g,getCellContent:d,columns:t.useMemo(()=>{const e=[...r];return e.length>6&&(e[6]={...e[6],title:"NUMBER",width:250}),e},[r]),rows:20,onCellEdited:i})};var n,a,o;l.parameters={...l.parameters,docs:{...(n=l.parameters)==null?void 0:n.docs,source:{originalSource:`() => {
+  const {
+    cols,
+    getCellContent,
+    setCellValue
+  } = useMockDataGenerator(7, false);
+  const _getCellContent = (cell: Item) => {
+    const [col, row] = cell;
+    if (col === 6) {
+      return {
+        kind: GridCellKind.Number,
+        data: 100,
+        displayData: "100",
+        readonly: false,
+        allowOverlay: true,
+        allowNegative: true
+      };
+    }
+    return getCellContent(cell);
+  };
+  return <DataEditor {...defaultProps} getCellContent={_getCellContent} columns={React.useMemo(() => {
+    const updated = [...cols];
+    if (updated.length > 6) {
+      updated[6] = {
+        ...updated[6],
+        title: "NUMBER",
+        width: 250
+      } as any;
+    }
+    return updated;
+  }, [cols])} rows={20} onCellEdited={setCellValue} />;
+}`,...(o=(a=l.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const O=["SmallEditableGrid"];export{l as SmallEditableGrid,O as __namedExportsOrder,B as default};
